2faf385b4074814ab8cf6a5ec06083da366274a8
Masa Maeda <masamaedae@gmail.com>
2024-02-19 16:04:12 -0800

    Finish initial implemention

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,9 @@ slint::slint! {
         min-width: 326px;
         min-height: 326px;
 
+        callback check_if_pair_solved();
+        in property <bool> disable_tiles;
+
         in property <[TileData]> memory_tiles: [
             { image: @image-url("icons/at.png") },
             { image: @image-url("icons/balance-scale.png") },
@@ -73,14 +76,62 @@ slint::slint! {
             // propagate the solved status from the model to the tile
             solved: tile.solved;
             clicked => {
-                tile.image_visible = !tile.image_visible;
+                if (!root.disable_tiles) {
+                    tile.image_visible = !tile.image_visible;
+                    root.check_if_pair_solved();
+                }
             }
         }
     }
 }
 
 fn main() -> Result<(), slint::PlatformError> {
+    use slint::Model;
+
     let ui = MainWindow::new()?;
+
+    // Fetch the tiles from the model
+    let mut tiles: Vec<TileData> = ui.get_memory_tiles().iter().collect();
+    // Duplicate them to ensure that we have pairs
+    tiles.extend(tiles.clone());
+
+    // Randomly mix the tiles
+    use rand::seq::SliceRandom;
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+
+    // Assign the shuffled Vec to the model property
+    let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
+    ui.set_memory_tiles(tiles_model.clone().into());
+
+    let ui_weak = ui.as_weak();
+    ui.on_check_if_pair_solved(move || {
+        let mut flipped_tiles =
+            tiles_model.iter().enumerate().filter(|(_, tile)| tile.image_visible && !tile.solved);
+
+        if let (Some((t1_idx, mut t1)), Some((t2_idx, mut t2))) =
+            (flipped_tiles.next(), flipped_tiles.next())
+        {
+            let is_pair_solved = t1 == t2;
+            if is_pair_solved {
+                t1.solved = true;
+                tiles_model.set_row_data(t1_idx, t1);
+                t2.solved = true;
+                tiles_model.set_row_data(t2_idx, t2);
+            } else {
+                let main_window = ui_weak.unwrap();
+                main_window.set_disable_tiles(true);
+                let tiles_model = tiles_model.clone();
+                slint::Timer::single_shot(std::time::Duration::from_secs(1), move || {
+                    main_window.set_disable_tiles(false);
+                    t1.image_visible = false;
+                    tiles_model.set_row_data(t1_idx, t1);
+                    t2.image_visible = false;
+                    tiles_model.set_row_data(t2_idx, t2);
+                });
+            }
+        }
+    });
 
     ui.run()
 }
